2fbf3c10381d303cf534f4403d4414ae45280456
Muh Zadi <zadiansyah@gmail.com>
2017-07-14 22:37:49 +0700

    Add files via upload

---
+++ Utils.java
@@ -0,0 +1,36 @@
+package com.zadi.kosakatabahasaarab.quiz;
+
+/**
+ * Created by Muh. Zadi on 6/18/2017.
+ */
+
+import android.support.v7.appcompat.BuildConfig;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Utils {
+    public static void TRACE(String TAG, String message)
+    {
+        if(BuildConfig.DEBUG){
+            Log.d(TAG, message);
+        }
+    }
+    public static void CopyStream(InputStream is, OutputStream os)
+    {
+        final int buffer_size=1024;
+        try{
+            byte[] bytes=new byte[buffer_size];
+            for(;;)
+            {
+                int count=is.read(bytes, 0, buffer_size);
+                if(count==-1)
+                    break;
+                os.write(bytes, 0, count);
+            }
+        }catch (Exception e){
+
+        }
+    }
+}
